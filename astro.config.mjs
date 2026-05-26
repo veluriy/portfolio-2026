313e401fb5979dea2bbc81bea5d926bd9e0a43d0
ptlx <ptlx@outlook.jp>
2026-05-26 13:04:10 +0900

    for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://veluriy.github.io',
+  base: '/portfolio-2026',
   integrations: [mdx()],
   markdown: {
     remarkPlugins: [remarkMath],
