c8e7eb2e32857b62f62335c82146eccf5973506a
ptlx <ptlx@outlook.jp>
2026-05-26 13:29:55 +0900

    add prefixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import rehypeKatex from 'rehype-katex';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://veluriy.github.io',
+  base: '/portfolio-2026',
   integrations: [mdx()],
   markdown: {
     remarkPlugins: [remarkMath],
